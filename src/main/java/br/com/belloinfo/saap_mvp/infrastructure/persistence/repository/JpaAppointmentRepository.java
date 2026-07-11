51c2306e2a55a34fce5e6a7ff53de613d05fcd96
Belicio B Cardoso <belloinfo@gmail.com>
2026-07-11 20:16:14 -0300

    refactor: remover importações não utilizadas nos repositórios de agendamento e criação de usuário

--- src/main/java/br/com/belloinfo/saap_mvp/infrastructure/persistence/repository/JpaAppointmentRepository.java
+++ src/main/java/br/com/belloinfo/saap_mvp/infrastructure/persistence/repository/JpaAppointmentRepository.java
@@ -12,7 +12,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaAppointmentRepository extends JpaRepository<AppointmentEntity, UUID> {
